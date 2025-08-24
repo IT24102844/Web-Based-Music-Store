9c4299ef45856a58b113f587ff3b264bd505d370
Sanusha <itssxnu@gmail.com>
2025-08-24 23:23:41 +0530

    Initial project setup

---
+++ src/main/java/com/app/musicstore/MusicStoreWebappApplication.java
@@ -0,0 +1,13 @@
+package com.app.musicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicStoreWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/musicstore/MusicStoreWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
